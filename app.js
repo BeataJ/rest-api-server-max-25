caed48fc11d697518c74dce66d575fc4c8195804
beata <beata@emc2wave.com>
2019-07-22 13:58:37 -0700

    body-parsser add getPost createPost create

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParsser = require('body-parser')
 const app = express();
+
+// app.use(bodyParsser.urlencoded({}));
 
 const feedRouter = require('./routes/feed')
 
