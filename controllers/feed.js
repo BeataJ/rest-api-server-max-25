d1ed94ebbac6a4ba3b0c49457983b51370a6b4be
beata <beata@emc2wave.com>
2019-07-22 14:34:06 -0700

    add post router modification createPost contller

--- controllers/feed.js
+++ controllers/feed.js
@@ -10,7 +10,7 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json()({
+    res.status(201).json({
         message: 'Post created successfully!',
         post: {
             id: new Date().toString(),
