cb7b2b1a3ea8b5a97f10772ea67d6457e4f8043d
beata <beata@emc2wave.com>
2019-07-24 13:58:47 -0700

    add post to createPost

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,39 +1,49 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'First Post',
-            content: 'This is the first post!',
-            imageUrl: 'images/gumball.png',
-            creator: {
-                name: 'Beata'
-            },
-            createdAt: new Date()
-        }]
-    });
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/gumball.png",
+        creator: {
+          name: "Beata"
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
 };
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: errors.array()
-        })
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-    // Create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: { name: 'Max'},
-            createdAt: new Date()
-        }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array()
+    });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    creator: { name: "Beata" }
+  });
+  post
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Post created successfully!",
+            post: result
+          });
     })
-}
+    .catch(err => {
+      console.log(err);
+    });
+  
+};
