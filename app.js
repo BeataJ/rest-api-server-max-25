2ca7347434eba437288686d9195b929964988ca8
beata <beata@emc2wave.com>
2019-07-24 12:59:35 -0700

    add db connection app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRouter = require('./routes/feed')
+const feedRouter = require("./routes/feed");
 
 const app = express();
 
@@ -9,12 +10,22 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.use('/feed', feedRouter);
+app.use("/feed", feedRouter);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://beata:1234@cluster0-b4yc7.mongodb.net/network?retryWrites=true&w=majority"
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
