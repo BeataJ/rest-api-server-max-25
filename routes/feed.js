cae9a18e2c100933b6e004c8196f8fbdc6effdba
beata <beata@emc2wave.com>
2019-07-23 18:00:17 -0700

    add validation to router post and controller create

--- routes/feed.js
+++ routes/feed.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
+const { check, body } = require("express-validator");
 
-const feedController = require('../controllers/feed');
+const feedController = require("../controllers/feed");
 
 const router = express.Router();
 //  GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ max: 5 }),
+    body("content")
+      .trim()
+      .isLength({ max: 5 })
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
