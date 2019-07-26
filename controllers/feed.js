e0998c95b52955b757796ad81546000820fc243f
beata <beata@emc2wave.com>
2019-07-26 16:19:36 -0700

    add validation to updatePost

--- controllers/feed.js
+++ controllers/feed.js
@@ -80,6 +80,12 @@ exports.getPost = (req, res, next) => {
 
 exports.updatePost = (req, res, next) => {
   const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;  
+  }
   const title = req.body.title;
   const content = req.body.content;
   let imageUrl = req.body.image;
@@ -91,4 +97,5 @@ exports.updatePost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  
 }
