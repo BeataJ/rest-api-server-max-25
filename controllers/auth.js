e2a09cc55d9d63120066f8fd20187e6ff83337ed
beata <beata@emc2wave.com>
2019-07-30 15:04:37 -0700

    add validationResult

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
+const { validationResult } = require("express-validator");
+
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        const error = new Error('Validation Failed.');
+        error.statusCode = 422;
+        error.data = error.array();
+        throw error;
+    }
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
