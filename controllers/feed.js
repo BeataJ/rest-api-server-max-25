75aeec20f23d507fe0893d691037f80581a5983f
beata <beata@emc2wave.com>
2019-07-31 16:02:51 -0700

    createPost modification

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -45,20 +46,29 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "Beata" }
+    creator: req.userId
   });
   post
     .save()
     .then(result => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then( user => {
+      creator = user;
+      user.posts.push(post);
+      user.save();
+    })
+    .then(result => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result
-      });
+        post: result,
+        creator: { _id: creator._id, name: creator.name}
+      })
     })
     .catch(err => {
       if (!err.statusCode) {
